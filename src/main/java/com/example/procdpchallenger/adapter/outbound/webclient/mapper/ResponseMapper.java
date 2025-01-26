6515ee26aeee8a0122d0560dca9ba478f7189652
n-k-neko <take70790@gmail.com>
2025-01-27 07:10:34 +0900

    doc:#10 バックエンド開発方針におけるWebクライアントについての説明を詳細化

--- src/main/java/com/example/procdpchallenger/adapter/outbound/webclient/mapper/ResponseMapper.java
+++ src/main/java/com/example/procdpchallenger/adapter/outbound/webclient/mapper/ResponseMapper.java
@@ -7,5 +7,5 @@ package com.example.procdpchallenger.adapter.outbound.webclient.mapper;
 public interface ResponseMapper<T, R> {
     R mapResponse(T response);  // 外部APIレスポンスをドメイン型に変換
     Class<T> getResponseType(); // 外部APIレスポンスの型を取得
-    Class<R> getDomainType();   // 対応するドメイン型を取得
+    Class<R> getDomainType();   // 対応するドメインの型を取得
 }
