848859c92820721d9e75d504bd69c5c06ca1486d
n-k-neko <take70790@gmail.com>
2025-02-13 07:36:23 +0900

    feature:#19 ロギング処理を実装、レスポンス作成処理をメソッド化

--- src/main/java/com/example/procdpchallenger/adapter/inbound/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/procdpchallenger/adapter/inbound/handler/GlobalExceptionHandler.java
@@ -13,38 +13,70 @@ import com.example.procdpchallenger.application.exception.BusinessRuleViolationE
 import com.example.procdpchallenger.domain.exception.DomainRuleViolationException;
 import com.example.procdpchallenger.infrastructure.exception.InfrastructureException;
 import com.example.procdpchallenger.infrastructure.exception.InvalidCredentialsException;
-
+import com.example.procdpchallenger.shared.exception.BaseException;
+import com.example.procdpchallenger.shared.exception.ErrorCategory;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(DomainRuleViolationException.class)
     public ResponseEntity<ErrorResponse> handleDomainException(DomainRuleViolationException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                             .body(new ErrorResponse(ex.getErrorCode(), ex.getMessage()));
+        logging(ex);
+        return createErrorResponse(ex);
     }
 
     @ExceptionHandler(BusinessRuleViolationException.class)
     public ResponseEntity<ErrorResponse> handleApplicationException(BusinessRuleViolationException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                             .body(new ErrorResponse(ex.getErrorCode(), ex.getMessage()));
+        logging(ex);
+        return createErrorResponse(ex);
     }
 
     @ExceptionHandler(InfrastructureException.class)
     public ResponseEntity<ErrorResponse> handleInfrastructureException(InfrastructureException ex) {
-        // エンドユーザーには一般化されたエラーメッセージを表示
-        logger.error("Infrastructure error occurred: {}", ex.getMessage(), ex);
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                             .body(new ErrorResponse(ex.getErrorCode(), "An unexpected error occurred."));
+        logging(ex);
+        return createErrorResponse(ex);
     }
 
     @ExceptionHandler(InvalidCredentialsException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED) 
-    public ResponseEntity<ErrorResponse> handleInvalidCredentialsException(InvalidCredentialsException e) {
+    public ResponseEntity<ErrorResponse> handleInvalidCredentialsException(InvalidCredentialsException ex) {
         ErrorResponse errorResponse = new ErrorResponse(
             "AUTHENTICATION_ERROR",
             "Invalid credentials provided"  // クライアントに返すメッセージ
         );
         return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
+    }
+
+    /**
+     * カスタム例外のログ出力
+     * @param ex 例外
+     */
+    public void logging(BaseException ex) {
+        if(ex.getErrorCategory() == ErrorCategory.ERROR) {
+            logger.error("Exception occurred: ErrorCategory={}, ErrorCode={}, Message={}", ex.getErrorCategory(), ex.getErrorCode(), ex.getMessage(), ex);
+        } else if(ex.getErrorCategory() == ErrorCategory.WARN) {
+            logger.warn("Exception occurred: ErrorCategory={}, ErrorCode={}, Message={}", ex.getErrorCategory(), ex.getErrorCode(), ex.getMessage(), ex);
+        } else {
+            // ログに記録しない
+        }
+    }
+
+    // TODO：一般例外のログ出力
+
+    /**
+     * エラーレスポンスの生成
+     * エラーカテゴリーが「エラー」は想定外のエラーのため、500エラーとしてエラーメッセージを一般化して返す
+     * エラーカテゴリーが「エラー」以外は、400エラーとしてエラーメッセージを返す
+     * @param ex 例外
+     * @return エラーレスポンス
+     */
+    public ResponseEntity<ErrorResponse> createErrorResponse(BaseException ex) {
+        if(ex.getErrorCategory() == ErrorCategory.ERROR) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                             .body(new ErrorResponse(ex.getErrorCode(), "An unexpected error occurred."));
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                             .body(new ErrorResponse(ex.getErrorCode(), ex.getMessage()));
+        }
     }
 }
